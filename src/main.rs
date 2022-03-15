04031b125b5ea7a931b198bb963a1129ec6a1476
Alexandra Negoita <93153280+AlexandraNegoita@users.noreply.github.com>
2022-03-15 15:27:13 +0200

    ex5 var2

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,62 @@
 use std::env;
 
-fn divide(a:i32,b:i32)->Option<i32>{
-    if b==0{
-        return None;
+fn divide(numbers: Vec<i32>)->Option<i32>{
+    let mut res=numbers[0];
+    // for index in 1..numbers.len(){
+    //     if *numbers[index]==0{
+    //         return None;
+    //     }
+    //     else{
+    //         res=res/numbers[index];
+    //     }
+    // }
+    for element in numbers.iter().skip(1) {
+        if *element==0{
+            return None;
+        }
+        else{
+            res=res/element;
+        }
     }
-    else{
-        return Some(a/b);
+    return Some(res);
+}
+fn rem(numbers: Vec<i32>)->Option<i32>{
+    let mut res=numbers[0];
+    for element in numbers.iter().skip(1) {
+        if *element==0{
+            return None;
+        }
+        else{
+            res=res%element;
+        }
     }
+    return Some(res);
 }
-fn rem(a:i32,b:i32)->Option<i32>{
-    if b==0{
-        return None;
+fn add(numbers: Vec<i32>)->i32{
+    let mut res=0;
+    for element in &numbers{
+        res+=element;
     }
-    else{
-        return Some(a%b);
+    return res;
+}
+fn substract(numbers: Vec<i32>)->i32{
+    let mut res=0;
+    for element in &numbers{
+        res-=element;
     }
+    return res;
 }
-fn add(a:i32,b:i32)->i32{
-    return a+b;
-}
-fn substract(a:i32,b:i32)->i32{
-    return a-b;
-}
-fn multiply(a:i32,b:i32)->i32{
-    return a*b;
+fn multiply(numbers: Vec<i32>)->i32{
+    let mut res=1;
+    for element in &numbers{
+        res*=element;
+    }
+    return res;
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len()!=3{
+    if args.len()<3{
         println!("Nu ati introdus parametri corespunzatori.");
         std::process::exit(-1);
     }
@@ -43,24 +71,38 @@ fn main() {
                 std::process::exit(-1);
             }
         };
-        let a=match args[1].parse(){
-            Ok(n) =>{
-                n
-            }
-            Err(_) => {
-                println!("A nu este un numar.");
-                std::process::exit(-1);
-            }
-        };
-        let b=match args[2].parse(){
-            Ok(n) =>{
-                n
-            }
-            Err(_) => {
-                println!("B nu este un numar.");
-                std::process::exit(-1);
-            }
-        };
+        let mut i=0;
+        let numbers: Vec<i32>;
+        for index in 1..args.len(){
+            numbers[i]=match args[index].parse(){
+                Ok(n) =>{
+                    n
+                }
+                Err(_) => {
+                    println!("Introduceti doar numere.");
+                    std::process::exit(-1);
+                }
+            };
+            i+=1;
+        }
+        // let a=match args[1].parse(){
+        //     Ok(n) =>{
+        //         n
+        //     }
+        //     Err(_) => {
+        //         println!("A nu este un numar.");
+        //         std::process::exit(-1);
+        //     }
+        // };
+        // let b=match args[2].parse(){
+        //     Ok(n) =>{
+        //         n
+        //     }
+        //     Err(_) => {
+        //         println!("B nu este un numar.");
+        //         std::process::exit(-1);
+        //     }
+        // };
         if command=="add"{
             println!("a + b = {}",add(a,b));
         }
